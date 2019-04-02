6de2325c6f072a7b904274731fd06eac23412d93
Lilinyang <1264576767@qq.com>
2019-04-02 21:40:13 +0800

    第一次提交

--- js/user/user.js
+++ js/user/user.js
@@ -9,8 +9,25 @@
 //     oTableInit.init = function (){
 //         $('table').bootstrapTable({
 //             url:'',   //请求后台url
-//             method:'',   //请求方式
-
+//             method:'post',   //请求方式
+//             toolbar: '#toolbar',    //启用工具栏
+//             striped: true,    //是否显示行间隔色
+//             pagination: true,   //是否分页
+//             queryParams:  {   //传递参数
+//                 pageNum:"1",
+//                 pagesize:"10"
+//             },
+//             pageNumber: 1,    //初始化加载第一页，默认第一页
+//             pageSize: 10,     //每页的记录行数（*）
+//             search: true,
+//             contentType: "application/x-www-form-urlencoded",
+//             columns: [{
+//                 checkbox: true
+//             },{
+//                     field:'',
+//                     title:''
+//                 }
+//             ]
 //         });
 //     }
 // }
